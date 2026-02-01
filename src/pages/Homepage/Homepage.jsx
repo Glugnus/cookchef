797fb19787967101f617b2a5be31cec8caaa3f0a
Glugnus <pernet.aurelien@gmail.com>
2026-02-01 22:35:08 +0100

    5-Pagination

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -8,6 +8,7 @@ function Homepage() {
   const [recipes, setRecipes] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [filter, setFilter] = useState("");
+  const [page, setPage] = useState(1);
   const BASE_URL_API = useContext(ApiContext);
 
   useEffect(() => {
@@ -15,10 +16,16 @@ function Homepage() {
     async function fetchRecipes() {
       try {
         setIsLoading(true);
-        const response = await fetch(BASE_URL_API);
+        const response = await fetch(
+          `${BASE_URL_API}?skip=${(page - 1) * 18}&limit=18`,
+        );
         if (response.ok && !cancel) {
-          const recipes = await response.json();
-          setRecipes(Array.isArray(recipes) ? recipes : [recipes]);
+          const newRecipes = await response.json();
+          setRecipes((x) =>
+            Array.isArray(newRecipes)
+              ? [...x, ...newRecipes]
+              : [...x, newRecipes],
+          );
         }
       } catch (e) {
         console.log("Erreur : ", e);
@@ -30,7 +37,7 @@ function Homepage() {
     }
     fetchRecipes();
     return () => (cancel = true);
-  }, []);
+  }, [BASE_URL_API, page]);
 
   function updateRecipe(updatedRecipe) {
     setRecipes(
@@ -45,7 +52,10 @@ function Homepage() {
 
   return (
     <div className={` flex-fill container p-20 d-flex flex-column`}>
-      <h1 className="my-30">Découvrez nos nouvelles recettes</h1>
+      <h1 className="my-30">
+        Découvrez nos nouvelles recettes{" "}
+        <small className={styles.small}> - {recipes.length}</small>
+      </h1>
       <div
         className={`card flex-fill d-flex flex-column mb-20 p-20 ${styles.contentCard}`}
       >
@@ -60,7 +70,7 @@ function Homepage() {
             placeholder="Rechercher"
           />
         </div>
-        {isLoading ? (
+        {isLoading && !recipes.length ? (
           <Loading />
         ) : (
           <div className={styles.grid}>
@@ -75,6 +85,11 @@ function Homepage() {
               ))}
           </div>
         )}
+        <div className="d-flex flex-row justify-content-center align-items-center p-20">
+          <button onClick={() => setPage(page + 1)} className="btn btn-primary">
+            Charger plus de recettes
+          </button>
+        </div>
       </div>
     </div>
   );
