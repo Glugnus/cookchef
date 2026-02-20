f80d288fd092d760771bf5f0a5b1120782daac2d
Glugnus <pernet.aurelien@gmail.com>
2026-02-20 09:20:35 +0100

    7-Liste des recettes Admin

--- src/pages/Admin/pages/AdminRecipes/AdminRecipes.jsx
+++ src/pages/Admin/pages/AdminRecipes/AdminRecipes.jsx
@@ -1,10 +1,10 @@
-import { Outlet } from "react-router-dom";
 import { Suspense } from "react";
+import { Outlet } from "react-router-dom";
 import AdminRecipesNav from "./components/AdminRecipesNav/AdminRecipesNav";
 
 function AdminRecipes() {
   return (
-    <div className="d-flex flex-column">
+    <div className="d-flex flex-column flex-fill">
       <h3 className="mb-20">Gestion des recettes</h3>
       <AdminRecipesNav></AdminRecipesNav>
       <div className="flex-fill d-flex flex-column">
